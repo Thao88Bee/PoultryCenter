c9f13d49688609dd47e2ad6eb9f8f91bd2946cf3
Thao88bee <beethao.bt@gmail.com>
2025-01-09 12:42:07 -0600

    UserAuthentication

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const { Model, Validator } = require('sequelize');
+const { Model, Validator } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
           len: [4, 30],
           isNotEmail(value) {
             if (Validator.isEmail(value)) {
-              throw new Error('Cannot be an email.');
+              throw new Error("Cannot be an email.");
             }
           },
         },
@@ -43,10 +43,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'User',
+      modelName: "User",
       defaultScope: {
         attributes: {
-          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
         },
       },
     }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const apiRouter = require('./api');
+const apiRouter = require("./api");
 
-router.use('/api', apiRouter);
+router.use("/api", apiRouter);
 
 // Hello World! Text
 router.get("/hello/world", function (req, res) {
