5db06e8e6e09f8a5936b34e3f0899d330b6c29d7
Thao88bee <beethao.bt@gmail.com>
2025-01-09 11:32:42 -0600

    AddAPIRouter

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get("/hello/world", function (req, res) {
-//   res.cookie("XSRF-TOKEN", req.csrfToken());
-//   res.send("Hello World!");
-// });
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+// Hello World! Text
+router.get("/hello/world", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  res.send("Hello World!");
+});
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
