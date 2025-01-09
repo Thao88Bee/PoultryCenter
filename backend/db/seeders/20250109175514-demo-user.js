b8fff4fd864fdae315784c0d1fa27f4d5d44ff8f
Thao88bee <beethao.bt@gmail.com>
2025-01-09 12:10:36 -0600

    AddUsersTable

---
+++ backend/db/seeders/20250109175514-demo-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('testing')
+      },
+      {
+        email: 'testing@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('testing')
+      },
+      {
+        email: 'bee@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('testing')
+      }
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
