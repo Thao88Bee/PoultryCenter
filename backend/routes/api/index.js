64099cdb7436ade01b9628622ed69f4411438c03
Thao88bee <beethao.bt@gmail.com>
2025-01-09 13:13:02 -0600

    UserAPIRoutes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,36 +1,47 @@
 const router = require("express").Router();
 
-const { User } = require("../../db/models");
-const { setTokenCookie } = require("../../utils/auth.js");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+
+// const { User } = require("../../db/models");
+// const { setTokenCookie } = require("../../utils/auth.js");
 
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 
-// GET /api/set-token-cookie
-router.get("/set-token-cookie", async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: "Demo-lition",
-    },
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
-});
+router.use("/session", sessionRouter);
 
-// GET /api/restore-user
-router.get("/restore-user", (req, res) => {
-  return res.json(req.user);
-});
+router.use("/users", usersRouter);
 
-// GET /api/require-auth
-const { requireAuth } = require("../../utils/auth.js");
-router.get("/require-auth", requireAuth, (req, res) => {
-  return res.json(req.user);
-});
-
-router.post("/test", function (req, res) {
+router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie
+// router.get("/set-token-cookie", async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: "Demo-lition",
+//     },
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// GET /api/restore-user
+// router.get("/restore-user", (req, res) => {
+//   return res.json(req.user);
+// });
+
+// GET /api/require-auth
+// const { requireAuth } = require("../../utils/auth.js");
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
