f3f2aabf6e51c12e6712eada21b3b897c24bb4f4
Thao88bee <beethao.bt@gmail.com>
2025-01-10 03:40:36 -0600

    UserRoute

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const loginRouter = require("./login.js");
+const signupRouter = require("./signup.js");
 
 // const { User } = require("../../db/models");
 // const { setTokenCookie } = require("../../utils/auth.js");
@@ -11,8 +13,9 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
-
 router.use("/users", usersRouter);
+router.use("/login", loginRouter);
+router.use("/signup", signupRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
