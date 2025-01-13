306e9b17d932ce3837fe82cc6aae55f65fa82e7d
Thao88bee <beethao.bt@gmail.com>
2025-01-12 18:35:41 -0600

    DeleteSwapMeetWatchlist

--- backend/routes/api/swapMeetWatchList.js
+++ backend/routes/api/swapMeetWatchList.js
@@ -4,4 +4,28 @@ const { requireAuth } = require("../../utils/auth");
 
 const router = express.Router();
 
+// Delete a Swap Meet Watchlist
+router.delete("/:swapMeetWatchListId", requireAuth, async (req, res, next) => {
+    const userId = parseInt(req.user.id)
+    const id = req.params.swapMeetWatchListId
+    const swapMeetWatchList = await SwapMeetWatchList.findByPk(id)
+
+    if (!swapMeetWatchList) {
+        res.status(404).json({
+            message: "Swap Meet Watchlist couldn't be found"
+        })
+    }
+
+    if (swapMeetWatchList.ownerId !== userId) {
+        res.status(403).json({
+            message: "Forbidden"
+        })
+    }
+
+    await swapMeetWatchList.destroy()
+    res.json({
+        message: "Successfully deleted"
+    })
+})
+
 module.exports = router;
