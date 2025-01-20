164d848142355e9c10468df5fabb65a122bffadf
Thao88bee <beethao.bt@gmail.com>
2025-01-20 06:39:39 -0600

    HomePage

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,9 +1,26 @@
 import AdvertisingSec from "./AdvertisingSec";
 import "./HomePage.css";
 import HomeSlider from "./HomeSlider";
-import show1 from "../../images/s1.jpg";
+import showOne from "../../images/s1.jpg";
+import swapOne from "../../images/taylor.jpg";
+import swapTwo from "../../images/taylor2.jpg";
+import swapThree from "../../images/taylor3.jpg";
+import swapFour from "../../images/pk.jpg";
+import swapFive from "../../images/taylor6.jpg";
+import swapSix from "../../images/taylor7.jpg";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const user = useSelector((state) => state.session.User);
+
+  const goTo = (e, page) => {
+    e.preventDefault();
+    navigate(`/${page}`);
+  };
+
   return (
     <>
       <div className="homePageWinner">
@@ -19,14 +36,26 @@ function HomePage() {
             <p>Share Idea&#39;s and Concern&#39;s</p>
             <p>or just Fun Facts</p>
           </div>
-          <img className="homePageShowImage" src={show1} alt="" />
+          <img className="homePageShowImage" src={showOne} alt="" />
+          <div className="homePageSwapSec">
+            <img className="homePageSwapImage" src={swapOne} alt="" />
+            <img className="homePageSwapImage" src={swapTwo} alt="" />
+            <img className="homePageSwapImage" src={swapThree} alt="" />
+            <img className="homePageSwapImage" src={swapFour} alt="" />
+            <img className="homePageSwapImage" src={swapFive} alt="" />
+            <img className="homePageSwapImage" src={swapSix} alt="" />
+          </div>
           <div>
-            <div className="homePageMember">
+            { !user ? (            <div className="homePageMember">
               <p>Become a PoultryCenter Member</p>
               <p>Post up Shows, Swap Meets</p>
               <p>So everyone get the lastest updates</p>
               <p>About yor Show, Swap Meets</p>
-            </div>
+              <br />
+              <button className="signupBtn" onClick={(e) => goTo(e, "signup")}>
+                Sign Up
+              </button>
+            </div>) : ("") }
           </div>
         </div>
       </div>
